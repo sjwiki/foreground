19144dedcf2504f36192b0065b639d7dc6f3aa9a
sjwiki <sjwiki@users.noreply.github.com>
2014-08-31 09:57:14 +0100

    Update Foreground.skin.php

--- Foreground.skin.php
+++ Foreground.skin.php
@@ -198,7 +198,7 @@ class foregroundTemplate extends BaseTemplate {
 						$displaytitle = str_replace($pagetitle, $newtitle, $displaytitle);
 					?><h4 class="namespace label"><?php print $namespace; ?></h4><?php } ?>
 					<h2 class="title"><?php print $displaytitle; ?></h2>
-					<?php if ( $this->data['isarticle'] ) { ?><h3 id="tagline"><?php $this->msg( 'tagline' ) ?></h3><?php } ?>
+					<!--<?php if ( $this->data['isarticle'] ) { ?><h3 id="tagline"><?php $this->msg( 'tagline' ) ?></h3><?php } ?>-->
 					<h5 class="subtitle"><?php $this->html('subtitle') ?></h5>
 					<div class="clear_both"></div>
 					<div class="mw-bodytext">
